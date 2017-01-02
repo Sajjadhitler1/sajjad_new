35c09a56790bd2ae6cee5effb160797ad0d80438
KiaRash <kia.pashang@gmail.com>
2017-01-02 23:08:10 +0000

    Add files via upload

---
+++ plugins/lock_arabic.lua
@@ -0,0 +1,59 @@
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_arabic'] then
+                lock_arabic = data[tostring(msg.to.id)]['settings']['lock_arabic']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_arabic == "yes" then
+    send_large_msg(get_receiver(msg), "Sending Arabic/Persian Words Are Forbidden\n\nCreated By @To_My_Amigos\nOur Channel @AntiSpam_TM")
+       delete_msg(msg.id, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+  
+  "^[چ](.*)$",
+  "^[ج](.*)$",
+  "^[ح](.*)$",
+  "^[خ](.*)$",
+  "^[ع](.*)$",
+  "^[ه](.*)$",
+  "^[غ](.*)$",
+  "^[ف](.*)$",
+  "^[ق](.*)$",
+  "^[ث](.*)$",
+  "^[ص](.*)$",
+  "^[ض](.*)$",
+  "^[ش](.*)$",
+  "^[س](.*)$",
+  "^[ی](.*)$",
+  "^[ب](.*)$",
+  "^[ل](.*)$",
+  "^[ا](.*)$",
+  "^[ت](.*)$",
+  "^[ن](.*)$",
+  "^[م](.*)$",
+  "^[ک](.*)$",
+  "^[گ](.*)$",
+  "^[پ](.*)$",
+  "^[و](.*)$",
+  "^[د](.*)$",
+  "^[ئ](.*)$",
+  "^[ذ](.*)$",
+  "^[ر](.*)$",
+  "^[ز](.*)$",
+  "^[ط](.*)$",
+  "^[ظ](.*)$",
+  
+  },
+  run = run
+} 
