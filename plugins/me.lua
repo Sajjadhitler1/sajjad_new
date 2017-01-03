978e84741953c144fd00fdaa9458bf43de59fb5e
KiaRash <kia.pashang@gmail.com>
2017-01-03 11:53:10 +0000

    Add files via upload

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,20 +1,20 @@
 --Created by: @To_My_Amigos
 
 local function run(msg, matches)
-  if matches[1]:lower() == 'semat' and not is_momod(msg) then
-     send_document(get_receiver(msg), "./semat/member.webp", ok_cb, false)
+  if matches[1]:lower() == 'me' and not is_momod(msg) then
+     send_document(get_receiver(msg), "./data/tmp/member.webp", ok_cb, false)
     return 'You Are A Member :/'
   end
-   if matches[1]:lower() == 'semat' and is_sudo(msg) then
-     send_document(get_receiver(msg), "./semat/sudo.webp", ok_cb, false)
+   if matches[1]:lower() == 'me' and is_sudo(msg) then
+     send_document(get_receiver(msg), "./data/tmp/sudo.webp", ok_cb, false)
     return 'You Are My Sudo ;)'
    end
-    if matches[1]:lower() == 'semat' and is_owner(msg) then
-     send_document(get_receiver(msg), "./semat/owner.webp", ok_cb, false)
+    if matches[1]:lower() == 'me' and is_owner(msg) then
+     send_document(get_receiver(msg), "./data/tmp/owner.webp", ok_cb, false)
     return 'You Are An Owner :)'
    end
-  if matches[1]:lower() == 'semat' and is_momod(msg) then
-     send_document(get_receiver(msg), "./semat/momod.webp", ok_cb, false)
+  if matches[1]:lower() == 'me' and is_momod(msg) then
+     send_document(get_receiver(msg), "./data/tmp/momod.webp", ok_cb, false)
     return 'You Are An Admin :)'
   end
 end
@@ -24,7 +24,7 @@ return {
 "CopyRight all right reserved",
   },
   patterns = {
-    "^[#!/]([Ss][Ee][Mm][Aa][Tt])$"
+    "^[#!/]([Mm][Ee])$"
   }, 
   run = run 
 }
